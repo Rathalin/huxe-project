93bd5302448d709445c77111d9a887b058e92bd6
Rathalin <danielflo1997@hotmail.com>
2022-10-08 16:32:11 +0200

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <p className="text-3xl font-bold underline">
+          Tailwind is working!
+        </p>
       </header>
     </div>
   );
