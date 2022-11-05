b5b782a69437384f2239588b12616b1edcf96218
Rathalin <danielflo1997@hotmail.com>
2022-11-05 16:40:22 +0100

    Fix bug in router where login guard was missing

--- react-app/src/components/routes/Router.tsx
+++ react-app/src/components/routes/Router.tsx
@@ -3,7 +3,6 @@ import { Dashboard } from '../Dashboard';
 import { StrongEmotion } from '../StrongEmotion';
 import { Register } from '../Register';
 import { Login } from '../Login';
-import { useStore } from '../../stores/useStore';
 import { useAuthStore } from '../../stores/authStore';
 import { NewMood } from '../NewMood';
 import { SelectEmotionType } from '../SelectEmotionType';
@@ -13,18 +12,13 @@ export const CustomRouter = () => {
 
   return (
     <Routes>
-      <Route path='/' element={loggedIn ? <Navigate to='/dashboard' /> : <Login />}
-      />
+      <Route path='/' element={loggedIn ? <Navigate to='/dashboard' /> : <Login />} />
       <Route path='dashboard' element={!loggedIn ? <Navigate to='/login' /> : <Dashboard />} />
-      <Route path='emotion' element={<StrongEmotion />} />
-      <Route path='login' element={loggedIn ? <Navigate to='/dashboard' /> : <Login />}
-      />
-      <Route path='newMood' element={!loggedIn ? <Navigate to='/login' /> : <NewMood />}
-      />
-      {/* <Route path='newEmotion' element={!loggedIn ? <Navigate to='/login' /> : <SelectEmotionType />}
-      /> */}
-      <Route path='register' element={loggedIn ? <Navigate to='/dashboard' /> : <Register />}
-      />
+      <Route path='emotion' element={!loggedIn ? <Navigate to='/login' /> : <StrongEmotion />} />
+      <Route path='login' element={loggedIn ? <Navigate to='/dashboard' /> : <Login />} />
+      <Route path='newMood' element={!loggedIn ? <Navigate to='/login' /> : <NewMood />} />
+      <Route path='newEmotion' element={!loggedIn ? <Navigate to='/login' /> : <SelectEmotionType />} />
+      <Route path='register' element={loggedIn ? <Navigate to='/dashboard' /> : <Register />} />
     </Routes>
   );
 };
