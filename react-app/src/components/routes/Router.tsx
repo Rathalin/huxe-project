d5aa4b8bf2c60f9585d8f4a6190a1ab372a78063
Lisa Lamplmair <lisa.lamplmair@gmx.at>
2022-11-07 10:29:26 +0100

    fix missing component in router

--- react-app/src/components/routes/Router.tsx
+++ react-app/src/components/routes/Router.tsx
@@ -6,7 +6,6 @@ import { Login } from '../Login';
 import { useAuthStore } from '../../stores/auth.store';
 import { NewMood } from '../NewMood';
 import { SelectEmotionType } from '../SelectEmotionType';
-import { NewEmotion } from '../NewEmotion';
 import { InitialPriorities } from '../InitialPriorities';
 
 export const CustomRouter = () => {
@@ -22,8 +21,6 @@ export const CustomRouter = () => {
       <Route path='login' element={loggedIn ? <Navigate to='/dashboard' /> : <Login />}
       />
       <Route path='newMood' element={!loggedIn ? <Navigate to='/login' /> : <NewMood />}
-      />
-      <Route path='newEmotion' element={!loggedIn ? <Navigate to='/login' /> : <NewEmotion />}
       />
       <Route path='initialPriorities' element={!loggedIn ? <Navigate to='/login' /> : <InitialPriorities />}
       />
