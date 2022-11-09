b83e9a99d23b383d08c8a637019f1610ff2481af
Rhatalin <danielflo1997@hotmail.com>
2022-11-09 20:39:55 +0100

    Fix bug where existing id was not returned

--- react-app/src/stores/dailyMoodStore.ts
+++ react-app/src/stores/dailyMoodStore.ts
@@ -3,15 +3,11 @@ import create from 'zustand';
 interface DailyMoodIdState {
   dailyMoodId: string | null,
   setDailyMoodId: (id: string) => void,
-  resetDailyMoodId: () => void,
 }
 
 export const useDailyMoodIdStore = create<DailyMoodIdState>(set => ({
   dailyMoodId: null,
   setDailyMoodId(id) {
     set({ dailyMoodId: id });
-  },
-  resetDailyMoodId() {
-    set({ dailyMoodId: null });
   },
 }));
